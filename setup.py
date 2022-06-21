27eb34761f9dbe60455b8a0d8f9d31dc076d8aed
Sebastian Lara Menares <slara@mtsolutions.io>
2022-06-21 12:01:40 -0400

    bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ except (IOError, OSError):
     CHANGES = ''
 
 setup(name='superslacker',
-      version='0.9',
+      version='0.9.1',
       license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
       description='superslacker plugin for supervisord',
       long_description=README + '\n\n' + CHANGES,
